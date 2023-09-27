4308792a5c0f4ff569836b3fdd1124fd2a14613c
Samuel Escapa <samuel@slingshot.fm>
2023-09-27 15:10:41 -0700

    fix: apply new design stylings [ENG-29] (#3)
    * fix: apply new design stylings [ENG-29]
    
    * add changeset

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -4,6 +4,9 @@ import type { FC } from 'react';
 import { Text } from 'paris/text';
 import { useMemo } from 'react';
 
+import clsx from 'clsx';
+import styles from 'src/components/ButtonGroup.module.scss';
+
 export type ButtonGroupOption = { id: string, name: string };
 
 export type ButtonGroupProps = {
@@ -25,12 +28,18 @@ export const ButtonGroup: FC<ButtonGroupProps> = ({
     ), [options, selected]);
 
     return (
-        <div className="relative flex p-1 py-2.5 space-x-1 bg-gray-200/50 rounded-full focus-within:outline-none focus-within:ring-1 focus-within:ring-offset-2 focus-within:ring-neutral-400">
+        <div
+            className={clsx(
+                styles.ButtonGroup,
+                'relative flex p-1 py-2.5 space-x-1 rounded-full focus-within:outline-none focus-within:ring-1 focus-within:ring-offset-2 focus-within:ring-neutral-400',
+            )}
+        >
             <div
                 className="absolute m-[4px] left-0 inset-y-0 w-1/3 flex bg-white transition-all ease-in-out duration-200 transform rounded-full shadow"
                 style={{
                     width: `calc(100% / ${options.length})`,
                     transform: `translateX(calc(${selectedIndex * 100}% - ${selectedIndex > 0 ? 8 : 0}px))`,
+                    boxShadow: 'var(--pte-lighting-subtlePopup)',
                 }}
             />
             {options.map((option, index) => (
@@ -40,7 +49,7 @@ export const ButtonGroup: FC<ButtonGroupProps> = ({
                     onClick={() => onChange?.(option)}
                     className="relative flex-1 flex text-xs sm:text-sm capitalize items-center justify-center m-px p-px focus-within:outline-none rounded-full"
                 >
-                    <Text kind="labelSmall" style={{ fontWeight: index === selectedIndex ? 600 : 400 }}>
+                    <Text kind="paragraphXSmall" style={{ fontWeight: 500 }}>
                         {option.name}
                     </Text>
                 </button>
