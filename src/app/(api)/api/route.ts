33f6223095efdc94c7297ffba59511d407e6792e
Sanil Chawla <hi@sanil.co>
2026-04-07 22:16:54 +0200

    feat: set API docs page title to app name

--- src/app/(api)/api/route.ts
+++ src/app/(api)/api/route.ts
@@ -1,6 +1,11 @@
 import { ApiReference } from '@scalar/nextjs-api-reference';
+import { HappinessConfig } from 'happiness.config';
 
 export const GET = ApiReference({
     url: '/api/openapi.json',
     theme: 'deepSpace',
+    pageTitle: `${HappinessConfig.name} API`,
+    metaData: {
+        title: `${HappinessConfig.name} API`,
+    },
 });
